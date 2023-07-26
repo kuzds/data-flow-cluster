68cab8ae39abc59065f5edbe0d0c1aab704aa9cb
kuznetsov_dms <kuznetsov_dms@tkbbank.ru>
2023-07-26 10:37:20 +0300

    Working on structure

--- camel-web2db/src/main/java/ru/kuzds/userflow/camel/web2db/route/Web2dbRoute.java
+++ camel-web2db/src/main/java/ru/kuzds/userflow/camel/web2db/route/Web2dbRoute.java
@@ -1,21 +1,37 @@
 package ru.kuzds.userflow.camel.web2db.route;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import ru.kuzds.userflow.camel.web2db.mapper.UserMapper;
 import ru.kuzds.userflow.userservice.SaveUserRequest;
 import ru.kuzds.userflow.userservice.SaveUserResponse;
 
+import javax.xml.bind.JAXBException;
+
 @Component
+@RequiredArgsConstructor
 public class Web2dbRoute extends RouteBuilder {
 
+    public static final String DR_SAVE_TO_DB = "direct:save-user-to-db";
+    public static final String DR_FROM_REST = "direct:from-rest";
+
+    public final UserMapper userMapper;
 
     @Value("${camel.rest.context-path}")
     private String contextPath;
 
     @Override
     public void configure() {
+
+        // XML Data Format
+//        JAXBContext con = JAXBContext.newInstance(SaveUserRequest.class);
+//        JaxbDataFormat xmlDataFormat = new JaxbDataFormat(con);
+
+//        // JSON Data Format
+//        JacksonDataFormat jsonDataFormat = new JacksonDataFormat(SaveUserResponse.class);
 
         restConfiguration()
             .component("servlet")
@@ -27,15 +43,25 @@ public class Web2dbRoute extends RouteBuilder {
 
         rest("/user")
             .post().type(SaveUserRequest.class).outType(SaveUserResponse.class)
-            .to("direct:save-user")
+            .to(DR_FROM_REST)
+        ;
+
+        from(DR_FROM_REST)
+            .wireTap(DR_SAVE_TO_DB)
+            .bean(userMapper, "toSaveUserResponse")
         ;
 
         from("cxf:bean:userEndpoint")
-            .log("SOAP body=${body}")
+            .convertBodyTo(SaveUserRequest.class)
+            .wireTap(DR_SAVE_TO_DB)
+            .bean(userMapper, "toSaveUserResponse")
         ;
 
-        from("direct:save-user")
-            .log("body: ${body}")
+        from(DR_SAVE_TO_DB)
+            .log("Saving to database body=${body}")
+//            .process(exchange -> {
+//                exchange.getIn().getBody();
+//            })
             .to("elsql:insertUser:sql/user.elsql")
         ;
     }
